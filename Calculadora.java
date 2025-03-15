b5913bc4ea42e8d93a55619e7728f706753ff48c
YuriFMX <110983706+YuriFMX@users.noreply.github.com>
2025-03-14 21:42:00 -0300

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,43 @@
+package info.java;
+
+public class Calculadora {
+    /**
+     * Método para somar valores.
+     * @param valor1 Primeiro valor inserido pelo usuário.
+     * @param valor2 Segundo valor inserido pelo usuário.
+     * @return Soma dos valor1 mais o valor2.
+     */
+    public double somar(final double valor1, final double valor2) {
+        return valor1 + valor2;
+    }
+
+    /**
+     * Método para subtrair valores.
+     * @param valor1 Primeiro valor inserido pelo usuário.
+     * @param valor2 Segundo valor inserido pelo usuário.
+     * @return Subtração dos valor1 menos o valor2.
+     */
+    public double subtrair(final double valor1, final double valor2) {
+        return valor1 - valor2;
+    }
+
+    /**
+     * Método para multiplicar valores.
+     * @param valor1 Primeiro valor inserido pelo usuário.
+     * @param valor2 Segundo valor inserido pelo usuário.
+     * @return Multiplicação dos valor1 vezes o valor2.
+     */
+    public double multiplicar(final double valor1, final double valor2) {
+        return valor1 * valor2;
+    }
+
+    /**
+     * Método para dividir valores.
+     * @param valor1 Primeiro valor inserido pelo usuário.
+     * @param valor2 Segundo valor inserido pelo usuário.
+     * @return Divisão do valor1 pelo valor2.
+     */
+    public double dividir(final double valor1, final double valor2) {
+        return valor1 / valor2;
+    }
+}
